f415b9217fc48401abccfa130dc9f680b9b9132f
Anupoju Mohana Rao <mohananupoju14@gmail.com>
2023-11-13 08:09:45 +0000

    CryptoCurrencyTracker-App

---
+++ src/components/CryptocurrencyTracker/index.js
@@ -0,0 +1,60 @@
+// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import CryptocurrenciesList from '../CryptocurrenciesList'
+
+import './index.css'
+
+const apiUrl = 'https://apis.ccbp.in/crypto-currency-converter'
+
+class CryptocurrencyTracker extends Component {
+  state = {
+    cryptocurrenciesData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getMainCryptoCurrency()
+  }
+
+  getMainCryptoCurrency = async () => {
+    const response = await fetch(apiUrl)
+    const totalData = await response.json()
+
+    this.setState({
+      cryptocurrenciesData: totalData.map(eachCryptocurrency => ({
+        id: eachCryptocurrency.id,
+        currencyLogoUrl: eachCryptocurrency.currency_logo,
+        currencyName: eachCryptocurrency.currency_name,
+        usdValue: eachCryptocurrency.usd_value,
+        euroValue: eachCryptocurrency.euro_value,
+      })),
+      isLoading: false,
+    })
+  }
+
+  renderMainCryptoList = () => {
+    const {cryptocurrenciesData} = this.state
+
+    return <CryptocurrenciesList cryptocurrenciesData={cryptocurrenciesData} />
+  }
+
+  renderLoader = () => (
+    <div data-testid="loader">
+      <Loader type="Rings" color="#ffffff" height={80} width={80} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="main-crypto-container">
+        {isLoading ? this.renderLoader() : this.renderMainCryptoList()}
+      </div>
+    )
+  }
+}
+
+export default CryptocurrencyTracker
